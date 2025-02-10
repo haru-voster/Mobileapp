6fd94e0a1ca4d407a933e3a3adb2353c1bf10f80
haru-voster <harounbravo2@gmail.com>
2025-02-10 16:18:41 +0200

    App development

---
+++ app/(tabs)/_layout.jsx
@@ -0,0 +1,38 @@
+import { View,Text } from "react-native";
+import React from "react";
+import { Tabs } from "expo-router";
+import FontAwesome from '@expo/vector-icons/FontAwesome';
+export default function TabLayout(){
+    return (
+      <Tabs screenOptions={{
+        headerShown:false
+      }}>
+        <Tabs.Screen name ='index'
+        options={{
+            tabBarLabel:"Home",
+            tabBarIcon:({color,size})=>(
+                <FontAwesome name="home" size={size} color="blue" />
+            )
+
+
+        }}
+        />
+        <Tabs.Screen name ='AddNew'
+        options={{
+            tabBarLabel:"Add New",
+            tabBarIcon:({color,size})=>(
+                <FontAwesome name="plus-square" size={size} color="blue" />
+            )
+        }}
+        />
+        <Tabs.Screen name ='Profile'
+         options={{
+            tabBarLabel:"Profile",
+            tabBarIcon:({color,size})=>(
+                <FontAwesome name="user" size={size} color="blue" />
+            )
+        }}/>
+
+      </Tabs>
+    )
+}
